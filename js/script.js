991025d29b2e000a5ca797914d8238153c3ed3cf
hrvieira <luizhrvieira@outlook.com>
2023-12-31 16:29:42 -0300

    Adicionei algumas mquerys para melhorar a responsividade, também trabalhei no menu hamburger, mas ainda não terminei.

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
 function toggleMenu() {
   var menu = document.querySelector('.menu');
   var menuToggle = document.querySelector('.menu-toggle');
-
+  
   menu.classList.toggle('active');
   menuToggle.classList.toggle('active');
+
 }
