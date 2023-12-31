b0747b6784e3d66600e80ed2b9ea87424e747b20
hrvieira <luizhrvieira@outlook.com>
2023-12-30 22:57:39 -0300

    Um portfólio um pouco mais bonito e apresentável!

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function toggleMenu() {
+  var menu = document.querySelector('.menu');
+  var menuToggle = document.querySelector('.menu-toggle');
+
+  menu.classList.toggle('active');
+  menuToggle.classList.toggle('active');
+}
