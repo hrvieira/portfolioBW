93169fcbd382bd187c1f75429f429e94feb5e452
hrvieira <luizhrvieira@outlook.com>
2024-02-25 21:04:54 -0300

    Ajeitando o menu toggle. Agora funciona de maneira melhor e com um design melhor

--- js/script.js
+++ js/script.js
@@ -1,8 +1,20 @@
-function toggleMenu() {
-  var menu = document.querySelector('.menu');
-  var menuToggle = document.querySelector('.menu-toggle');
-  
-  menu.classList.toggle('active');
-  menuToggle.classList.toggle('active');
+const btnMobile = document.getElementById('btn-mobile');
 
+function toggleMenu(event) {
+  if(event.type === 'touchstart') event.preventDefaul();
+  const nav = document.getElementById('nav');
+  nav.classList.toggle('active');
+
+  const active = nav.classList.contains('active');
+  event.currentTarget.setAttribute('aria-expanded', active);
+
+  if (active){
+    event.currentTarget.setAttribute('aria-label', 'Fechar menu');
+  } else {
+    event.currentTarget.setAttribute('aria-label', 'Abir menu');
+  }
+  
 }
+
+btnMobile.addEventListener('click', toggleMenu);
+btnMobile.addEventListener('touchstart', toggleMenu);
